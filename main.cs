766e745b50486c7e788327c8e3b284c1fc9b444e
MrC0de <mrc0de@geekprojex.com>
2020-12-24 06:35:20 -0700

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,21 @@
+module.exports.loop = function () {
+    // requires
+    let _ = require('lodash');
+    // let spawner = require('spawn.cs');
+    // let screeper = require('screep.cs');
+    
+    /////////////
+    // dig graves
+    _.forEach(Memory.creeps, (obj,k) => {
+        let c = JSON.stringify(k); // the hashkey (ie: its his name)
+       if ( !Game.creeps[k]) {
+            delete Memory.creeps[k];
+            console.log('Buried ' + c + '. They were dead.');
+            // Game.notify('Buried ' + c + '. They were dead.',5);
+        } 
+    });
+
+    /////////////
+    //
+    /////////////
+}
